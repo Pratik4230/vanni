30adefc7057515efc34a8eabdef4c34790dac77f
Pratik4230 <pratikjadhav1438@gmail.com>
2026-03-16 18:52:51 +0530

    Take input and process

--- src/services/liveAudioManager.ts
+++ src/services/liveAudioManager.ts
@@ -1,4 +1,5 @@
-import { MODEL } from '@/lib/constants';
+import { createPCMBlob } from '@/lib/audioUtils';
+import { INPUT_SAMPLE_RATE, MODEL, OUTPUT_SAMPLE_RATE } from '@/lib/constants';
 import { GoogleGenAI, Modality, Session } from '@google/genai';
 
 
@@ -6,10 +7,15 @@ export class LiveAudioManager {
 
     private ai: GoogleGenAI;
     private activeSession: Session | null = null
-
+    private inputAudioContext: AudioContext | null = null
+    private outputAudioContext: AudioContext | null = null
+    private outputNode : GainNode | null = null
+    private mediaStream : MediaStream | null = null
+    private workletNode: AudioWorkletNode | null = null
+    private inputSource: MediaStreamAudioSourceNode | null = null
     constructor() {
         this.ai = new GoogleGenAI({
-apiKey:"AIzaSyBY7AZ8lZ9bO89pihnZcxqrdGtnoU9FoBs"
+                 apiKey:process.env.NEXT_PUBLIC_GEMINI_API_KEY
         })
     }
 
@@ -30,6 +36,63 @@ apiKey:"AIzaSyBY7AZ8lZ9bO89pihnZcxqrdGtnoU9FoBs"
             onclose: (e) => console.log("Close  Gemini ", e.reason),
         }
     })
+
+    this.inputAudioContext = new AudioContext({
+        sampleRate: INPUT_SAMPLE_RATE
+    })
+        this.outputAudioContext = new AudioContext({
+            sampleRate: OUTPUT_SAMPLE_RATE
+        })
+
+
+        if (this.inputAudioContext.state === "suspended") {
+            this.inputAudioContext.resume()
+        }
+
+         if (this.outputAudioContext.state === "suspended") {
+            this.outputAudioContext.resume()
+        }
+
+        this.outputNode = this.outputAudioContext.createGain();
+        this.outputNode.connect(
+            this.outputAudioContext.destination,
+        );
+
+        await this.inputAudioContext.audioWorklet.addModule(
+            "/worklets/mic-processor.js",
+        );
+
+        this.workletNode = new AudioWorkletNode(
+            this.inputAudioContext, "mic-processor"
+        );
+
+
+        this.workletNode.port.onmessage = (event) => {
+            const pcmBlob = createPCMBlob(
+                event.data as Float32Array
+            );
+
+            this.activeSession?.sendRealtimeInput({
+                audio: pcmBlob
+            });
+        };
+
+        this.mediaStream = await navigator.mediaDevices.getUserMedia({
+            audio: {
+                sampleRate: INPUT_SAMPLE_RATE,
+                channelCount: 1,
+                echoCancellation: true,
+                noiseSuppression: true,
+                autoGainControl: true
+            }
+        });
+
+     this.inputSource = this.inputAudioContext.createMediaStreamSource(
+            this.mediaStream
+        );
+
+    this.inputSource.connect(this.workletNode);
+
     }
 }
 
